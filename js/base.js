fc0121f66f36d122e89835d5be3568162dc05cfe
Your Nameqiuqingyun <qiuqingyun98@outlook.com>
2018-09-26 13:58:05 +0800

    播放菜单小改

--- js/base.js
+++ js/base.js
@@ -1,6 +1,18 @@
+yearInput();
+// mobilSet();
 function yearInput(){
   var data = new Date();
   var copy=document.getElementsByClassName('copy');
   copy[0].innerHTML='&copy; '+data.getFullYear()+' By Qing';
 }
-yearInput();
+
+//px to rem
+function px2rem(pxs){
+  var rems=pxs/20+'rem';
+  return rems;
+}
+function mobilSet(){
+  var visualViewport=window. innerWidth;
+  var mainContainer=document.getElementsByClassName('mainContainer')[0];
+  mainContainer.style.width=px2rem(visualViewport);
+}
